532b1f1a09cc15303c8558b3d87701a5eb01f4b4
ogulcancevik <cancevdev@gmail.com>
2024-02-17 16:47:47 +0300

    feat: timezone service and types added.

--- src/services/timezone.service.ts
+++ src/services/timezone.service.ts
@@ -1,6 +1,16 @@
+import { Time } from '../types/timezone.types'
 import { api } from './api'
 
+export const fetchMyTime = async () => {
+  const { data } = await api.get<Time>('/ip')
+  return data
+}
 export const fetchTimezone = async () => {
   const { data } = await api.get<string[]>('/timezone')
+  return data
+}
+
+export const fetchTimezoneBySearch = async (timezone: string) => {
+  const { data } = await api.get<Time>(`/timezone/${timezone}`)
   return data
 }
