abef514bfba57276b956dde4c584e64c93b53bec
ogulcancevik <cancevdev@gmail.com>
2024-02-17 16:47:17 +0300

    feat: added tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' },
+      xs: { max: '479px' },
+    },
   },
   plugins: [],
 }
